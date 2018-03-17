5194b80734446fd83f25340c291d0adaf3edc901
Matthew Alicea <aliceami@appstate.edu>
2018-03-16 20:40:13 -0400

    Sass integreation setup

--- matthew-ia/Gulpfile.js
+++ matthew-ia/Gulpfile.js
@@ -1,13 +1,27 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 
-gulp.task('styles', function() {
-    gulp.src('styles/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles/css/'));
+gulp.task('sass', function() {
+    return gulp.src('src/styles/sass/style.scss')
+        // Minify
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('src/styles/css/'))
+
+        // Normal compile with sourcemap
+        .pipe(sourcemaps.init())
+        .pipe(sass()
+            .on('error', sass.logError)
+        )
+        .pipe(concat('style.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/styles/css/'))
 });
 
-//Watch task
+// Watch task
 gulp.task('default',function() {
-    gulp.watch('styles/**/*.scss',['styles']);
+    gulp.watch('src/styles/sass/**/*.scss',['sass']);
 });
