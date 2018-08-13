feacecd42cc3eb5f657ababeaef0cb9565126335
Matthew Alicea <aliceami@appstate.edu>
2018-08-13 13:31:50 -0400

    Fixed #5 – Added default route
    The info page acts as the default route, and the route for garbage urls.

--- matthew-ia/src/components/Main/Main.js
+++ matthew-ia/src/components/Main/Main.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from "react";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ProjectList from '../ProjectList/ProjectList';
 import ProjectDetail from '../ProjectDetail/ProjectDetail';
 import Home from '../Home/Home';
@@ -17,9 +17,13 @@ class Main extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/" render={() => ( /* Handles default route */
+          <Redirect to="/info"/>
+        )}/>
         <Route exact path='/info' component={Home} />
         <Route exact path='/projects' component={ProjectList} />
         <Route path='/projects/:number' component={ProjectDetail} />
+        <Route component={Home} /> {/* Handles garbage slugs */}
       </Switch>
     );
   }
