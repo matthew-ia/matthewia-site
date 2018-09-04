a7e803841ceec357213b13064196a46743fc9288
Matthew Alicea <aliceami@appstate.edu>
2018-09-04 16:11:47 -0400

    Reorganized Spectra detail, added label/text style

--- matthew-ia/src/components/ProjectDetail/projects/Spectra/_Gallery.js
+++ matthew-ia/src/components/ProjectDetail/projects/Spectra/_Gallery.js
@@ -21,30 +21,24 @@ export class _Gallery extends Component {
         <div>
           <div id="t2016" className="col gallery-marker">
             <Image onLoad={this.props.setColumnWidth}
-                   className='md'
+                   className='sm'
                    id='t2016'
                    path={p.publicPath}
                    previewFile={'spectra-v0.0-a.png'}
                    fullscreenFile={'spectra-v0.0-a.png'}/>
-            <p className="stacked">From the beginning, we knew we wanted the interface to feel like a space to explore. Thus, we also needed to think about organizing that space in an intuitive way.</p>
-          </div>
-          <div className='col'>
-            <Image onLoad={this.props.setColumnWidth}
-                   className='sm'
+            <Image className='sm'
                    path={p.publicPath}
                    previewFile={'spectra-v0.0-b.png'}
                    fullscreenFile={'spectra-v0.0-b.png'}/>
-            <Image className='sm'
-                   path={p.publicPath}
-                   previewFile={'spectra-v0.0-c.png'}
-                   fullscreenFile={'spectra-v0.0-c.png'}/>
           </div>
           <div className='col'>
-            <Image className='md'
+            <Image onLoad={this.props.setColumnWidth}
+                   className='md'
                    path={p.publicPath}
                    previewFile={'spectra-v0.0-d.png'}
                    fullscreenFile={'spectra-v0.0-d.png'}/>
             <Label text="Early mockup of Spectrum Search view"/>
+            <p className="stacked withLabel">From the beginning, we knew we wanted the interface to feel like a space to explore. Thus, we also needed to think about organizing that space in an intuitive way.</p>
           </div>
           <div id="t2017" className="col gallery-marker">
             <Image onLoad={this.props.setColumnWidth}
@@ -58,27 +52,6 @@ export class _Gallery extends Component {
           </div>
           <div className='col'>
             <Image onLoad={this.props.setColumnWidth}
-                   className='xs'
-                   path={p.publicPath}
-                   previewFile={'spectra-interaction-spectrum.png'}
-                   fullscreenFile={'spectra-interaction-spectrum.png'}/>
-            <Label text="Spectrum interaction"/>
-            <Image className='xs'
-                   path={p.publicPath}
-                   previewFile={'spectra-interaction-article.png'}
-                   fullscreenFile={'spectra-interaction-article.png'}/>
-            <Label text="Article interaction (previously referred to as &quot;Items&quot;)"/>
-          </div>
-          <div className='col'>
-            <Image onLoad={this.props.setColumnWidth}
-                   className='md'
-                   path={p.publicPath}
-                   previewFile={'dashboard.png'}
-                   fullscreenFile={'dashboard.png'}/>
-            <Label text="Dashboard view"/>
-          </div>
-          <div className='col'>
-            <Image onLoad={this.props.setColumnWidth}
                    className='sm'
                    path={p.publicPath}
                    previewFile={'dashboard-my-spectra.png'}
@@ -88,7 +61,15 @@ export class _Gallery extends Component {
                    path={p.publicPath}
                    previewFile={'dashboard-my-spectra-iteration.png'}
                    fullscreenFile={'dashboard-my-spectra-iteration.png'}/>
-            <Label text="Settings view iteration"/>
+            <Label text="My Spectrum view iteration"/>
+          </div>
+          <div className='col'>
+            <Image onLoad={this.props.setColumnWidth}
+                   className='md'
+                   path={p.publicPath}
+                   previewFile={'dashboard.png'}
+                   fullscreenFile={'dashboard.png'}/>
+            <Label text="Dashboard view"/>
           </div>
           <div className='col gallery-marker' id='t2018'>
             <Video videoSrc='https://www.youtube.com/embed/cdeOKFJA3MU?rel=0&amp;showinfo=0&mute=1'
