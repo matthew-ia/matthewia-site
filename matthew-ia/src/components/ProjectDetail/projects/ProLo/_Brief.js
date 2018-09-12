6234b24fbbd85270054c21ca55cedb054710f595
Matthew Alicea <aliceami@appstate.edu>
2018-09-12 18:00:07 -0400

    Fixed a tags on ProLo Systems detail

--- matthew-ia/src/components/ProjectDetail/projects/ProLo/_Brief.js
+++ matthew-ia/src/components/ProjectDetail/projects/ProLo/_Brief.js
@@ -18,10 +18,10 @@ export class _Brief extends Component {
         <div className="p-desc">
           <h1 className="p-title"> {p.info.name }</h1>
           <span className="p-tags">{p.info.tags.join(" // ") }</span>
-          <p className="p-content">For the final project in my client-side web programming course, my group and I created a prototype business management tool to help keep track of property declarations for taxes. I designed the UI, and worked collaboratively to program the application using JavaScript and JQuery. Working on this project was my first experience leading a collaborative programming effort. I set up the project and wrote <a href="https://github.com/matthew-ia/prolo-systems/blob/master/README.md">an extensive README</a> with the goal of getting us started by translating my design work into code, and outlining features and specs.
+          <p className="p-content">For the final project in my client-side web programming course, my group and I created a prototype business management tool to help keep track of property declarations for taxes. I designed the UI, and worked collaboratively to program the application using JavaScript and JQuery. Working on this project was my first experience leading a collaborative programming effort. I set up the project and wrote <a href="https://github.com/matthew-ia/prolo-systems/blob/master/README.md" target='_blank'>an extensive README</a> with the goal of getting us started by translating my design work into code, and outlining features and specs.
           </p>
           <ul id='project-links'>
-            <li><a href="https://github.com/matthew-ia/matthewia-site/issues/57">>> GitHub –  ProLo Systems</a></li>
+            <li><a href="https://github.com/matthew-ia/matthewia-site/issues/57" target='_blank'>>> GitHub –  ProLo Systems</a></li>
           </ul>
         </div>
         <Image className='p-image'
