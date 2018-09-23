3429652bf69704129cc1e3d996a6bd0a4bbac643
Matthew Alicea <aliceami@appstate.edu>
2018-09-22 20:32:30 -0400

    Fixed #69 – added PDF for DGSF proposal

--- matthew-ia/src/components/ProjectDetail/projects/DGSF/_Brief.js
+++ matthew-ia/src/components/ProjectDetail/projects/DGSF/_Brief.js
@@ -21,7 +21,7 @@ export class _Brief extends Component {
           <p className="p-content">In 2013 I was given the opportunity to build a website for the Doris Goedeke Scholarship Foundation (DGSF), a non-profit that helps fund college experiences for Hispanic youth. I worked on a volunteer basis to build the site, and then maintained it until early 2018. This project sparked my interest in computer science and web design as a career option. It wasn’t until 2017 that I started looking back to the project and thinking about how it could be improved with my new skillset.
           </p>
           <ul id='project-links'>
-            <li><a href="https://github.com/matthew-ia/matthewia-site/issues/57"
+            <li><a href={window.location.origin + "/docs/redesign-proposal.pdf"}
                    target='_blank'>>> Redesign_Proposal.pdf</a></li>
           </ul>
         </div>
