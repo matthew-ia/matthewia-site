a42099cd8036085160c98bb0750f5b68b0f15679
Matthew Alicea <aliceami@appstate.edu>
2018-09-26 16:37:37 -0400

    Fixed Project List centering on mobile

--- matthew-ia/src/components/ProjectList/ProjectList.js
+++ matthew-ia/src/components/ProjectList/ProjectList.js
@@ -32,7 +32,7 @@ class ProjectList extends Component {
       projects.push(<li id={ "" + key }
                         className={"p-item" + ' ' + props.projects[key].listTextStyle}>
         <Link to={ '/projects/' + pid }>
-          <div id="p-id">{ padNum(pid)} </div>
+          <div className="p-id">{ padNum(pid)} </div>
           <img className="p-image" src={ window.location.origin + '/images/p' + pid + '/list-preview.png'} alt={ "Project " + pid } />
           <div className="p-label">
             <h3>{ props.projects[key].name }</h3>
