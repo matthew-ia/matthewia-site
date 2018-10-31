5c6167ae6af4ee75171420112eb4445c6ad120b9
Matthew Alicea <aliceami@appstate.edu>
2018-10-30 22:00:36 -0400

    Fixed #90 – 2 years, not 22

--- matthew-ia/src/components/ProjectDetail/projects/Spectra/_Brief.js
+++ matthew-ia/src/components/ProjectDetail/projects/Spectra/_Brief.js
@@ -18,7 +18,7 @@ export class _Brief extends Component {
         <div className="p-desc">
           <h1 className="p-title"> {p.info.name }</h1>
           <span className="p-tags">{p.info.tags.join(" // ") }</span>
-          <p className="p-content">Spectra is a search engine that visualizes ideas in a way that popular search engines do not. With Spectra, you search the web for ideas and concepts. This is a search engine for knowledge; not just for websites, or images, but for exploring entire systems of thought. I’ve worked on Spectra for over 22 years collaboratively with another designer, slowly conceptualizing and iterating.<br/><br/>
+          <p className="p-content">Spectra is a search engine that visualizes ideas in a way that popular search engines do not. With Spectra, you search the web for ideas and concepts. This is a search engine for knowledge; not just for websites, or images, but for exploring entire systems of thought. I’ve worked on Spectra for over 2 years collaboratively with another designer, slowly conceptualizing and iterating.<br/><br/>
             Nothing is one thing, but rather a sum of its parts. Spectra aims to emulate that essence. No idea or concept exists in a vacuum separate from all other concepts, but rather is intertwined with others in order to exist. Spectra aims to let users explore that by focusing on three things: visualization, navigation, and organization.<br/><br/>
             For my senior capstone, I developed an early version of Spectra.
           </p>
